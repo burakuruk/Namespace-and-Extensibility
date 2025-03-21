8a1312471f6a532156e80dbc08fdaeb414d312af
burak uruk <burakurruk@gmail.com>
2025-03-21 17:39:23 +0200

    Proje dosyası ekle.

---
+++ Namespace-and-Extensibility/Classes/Hesaplama.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Namespace_and_Extensibility.Classes
+{
+    namespace MatematikIslemleri
+    {
+        public class Hesaplama
+        {
+            public int Topla(int sayi1, int sayi2)
+            {
+                return sayi1 + sayi2;
+            }
+
+            public int Carp(int sayi1, int sayi2)
+            {
+                return sayi1 * sayi2;
+            }
+        }
+    }
+}
---
+++ Namespace-and-Extensibility/Program.cs
@@ -0,0 +1,18 @@
+﻿using Namespace_and_Extensibility.Classes.MatematikIslemleri;
+
+namespace UygulamaCalistirici
+{
+    class Program
+    {
+        static void Main()
+        {
+            Hesaplama hesap = new Hesaplama();
+
+            int toplam = hesap.Topla(15, 5);
+            int carpim = hesap.Carp(6, 7);
+
+            Console.WriteLine("Toplam: " + toplam);
+            Console.WriteLine("Çarpım: " + carpim);
+        }
+    }
+}
